bb2618f88e479ef26f1124f4d9bc45f0557064f0
Subhash-Dhandhukiya <subhashdhandhukiya69@gmail.com>
2021-07-20 12:03:00 +0530

    Drawer Navigation Added

---
+++ src/navigation/Drawer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { View, Text, useWindowDimensions } from 'react-native'
+
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { NavigationContainer } from '@react-navigation/native'
+import { FIRSTSCREEN, SECONDSCREEN } from '../Constant/Route'
+import { FirstScreen, SecondScreen } from '../screens'
+
+
+const Drawer = createDrawerNavigator()
+
+const MyDrawer = () => {
+
+    const dimention = useWindowDimensions()
+
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator
+                drawerType={dimention.width >= 768 ? 'slide' : 'front'}
+                initialRouteName={FIRSTSCREEN}
+                backBehavior="history"
+            >
+                <Drawer.Screen
+                    name={FIRSTSCREEN}
+                    component={FirstScreen}
+                    options={{drawerLabel:'Home'}}
+                />
+
+                <Drawer.Screen
+                    name={SECONDSCREEN}
+                    component={SecondScreen}
+                    options={{drawerLabel:'Search'}}
+                />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default MyDrawer
