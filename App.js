463d06114b7e430ebfb4b5aa23c6fe85a121f40e
Subhash-Dhandhukiya <subhashdhandhukiya69@gmail.com>
2021-07-20 12:03:59 +0530

    Update code

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import NavContainer from './src/navigation/Stack';
 import BottomTab from './src/navigation/BottomTab';
+import Drawer from './src/navigation/Drawer';
 
 
 const App = () => {
   return (
-    <BottomTab/>
+    <Drawer/>
   )
 }
 
